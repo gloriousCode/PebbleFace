71fec0997176043fd7585f46d3a783de77cf2041
Scott <gloriousCode@users.noreply.github.com>
2015-06-29 08:31:28 +1000

    new font, no semicolons
    Added new block font for time. Removed semicolon in time rendering

--- src/main.c
+++ src/main.c
@@ -41,7 +41,7 @@ static void main_window_load(Window *window) {
   s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
-  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_text(s_time_layer, "00 00");
   
     // Create task TextLayer
   s_task_layer = text_layer_create(GRect(5, 5, 100, 50));
@@ -50,7 +50,7 @@ static void main_window_load(Window *window) {
   text_layer_set_text(s_task_layer, "STUDY");
 
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_MEDIUM_40));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_40));
   s_task_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_THIN_18));
   // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
@@ -88,11 +88,11 @@ static void update_time() {
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
     // Use 24 hour format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    strftime(buffer, sizeof("0000"), "%H%M", tick_time);
     update_task(buffer);
   } else {
     // Use 12 hour format
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    strftime(buffer, sizeof("0000"), "%I%M", tick_time);
   }
   //char[] codeTime = "16:30";
   
