b091865578ee5683a9adf539a97802b1343b1e7f
Scott <gloriousCode@users.noreply.github.com>
2015-07-06 21:39:43 +1000

    HMM INTERESTING!
    Getting a strange error in that I cannot install the app since making a function to return a char. Will investigate soon.
    
    Otherwise, changes include
    - Three new text layers
    - Calculation of hours, days, minutes ints in their own methods
    - Moving towards cool text based time instead of lame-ass digital numbers

--- src/main.c
+++ src/main.c
@@ -3,6 +3,11 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_task_layer;
+
+static TextLayer *s_row_one_layer;
+static TextLayer *s_row_two_layer;
+static TextLayer *s_row_three_layer;
+
 static GFont s_time_font;
 static GFont s_task_font;
 static Layer *s_path_layer;
@@ -11,6 +16,44 @@ static GPathInfo PATH_INFO = {
   .num_points = 4,
   .points = (GPoint[]) { {00, 60},  {220, 60}, {220, 100}, {00, 100} }
 };
+
+//Important mini methods to get the int values of time
+static int get_minutes(struct tm *tick_time) {
+  static char minuteStr[] = "00";
+  strftime(minuteStr, sizeof("00"), "%M", tick_time);
+  int minutes = atoi(minuteStr); 
+  
+  return minutes;
+}
+
+static int get_hours(struct tm *tick_time) {
+  static char hoursStr[] = "00";
+  strftime(hoursStr, sizeof("00"), "%H", tick_time);
+  int hours = atoi(hoursStr);
+  
+  return hours;
+}
+
+static int get_days(struct tm *tick_time) {
+  static char dayStr[] = "0";
+  strftime(dayStr, sizeof("0"), "%w", tick_time);
+  int day = atoi(dayStr);
+  
+  return day;
+}
+
+static char* get_hour_str(struct tm *tick_time) 
+{
+  static char hoursStr[] = "000000";
+  strftime(hoursStr, sizeof("00"), "%I", tick_time);
+  int hours = atoi(hoursStr);
+  if(hours == 1){
+    snprintf(hoursStr, 3, "ONE");
+  }
+  
+  return hoursStr;
+}
+
 
 //Method to draw layer
 static void layer_update_proc(Layer *layer, GContext *ctx) {
@@ -33,7 +76,7 @@ static void declare_drawing_layer(Window *window) {
 }
 static void set_fonts() {
     // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_40));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_44));
   s_task_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_THIN_18));
 }
 
@@ -47,18 +90,42 @@ static void declare_text_layers(Window *window) {
   text_layer_set_background_color(s_task_layer, GColorClear);
   text_layer_set_text_color(s_task_layer, GColorWhite);
   
+  s_row_one_layer = text_layer_create(GRect(5, 40, 100, 50));
+  text_layer_set_background_color(s_row_one_layer, GColorClear);
+  text_layer_set_text_color(s_row_one_layer, GColorWhite);
+  
+    s_row_two_layer = text_layer_create(GRect(5, 55, 100, 50));
+  text_layer_set_background_color(s_row_two_layer, GColorClear);
+  text_layer_set_text_color(s_row_two_layer, GColorWhite);
+  
+    s_row_three_layer = text_layer_create(GRect(5, 70, 100, 50));
+  text_layer_set_background_color(s_row_three_layer, GColorClear);
+  text_layer_set_text_color(s_row_three_layer, GColorWhite);
+  
   set_fonts();
   
     // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_font(s_task_layer, s_task_font);
   
+  text_layer_set_font(s_row_one_layer, s_task_font);
+  text_layer_set_font(s_row_two_layer, s_task_font);
+  text_layer_set_font(s_row_three_layer, s_task_font);
+  
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   text_layer_set_text_alignment(s_task_layer, GTextAlignmentCenter);
+  
+  text_layer_set_text_alignment(s_row_one_layer, GTextAlignmentLeft);
+  text_layer_set_text_alignment(s_row_two_layer, GTextAlignmentLeft);
+  text_layer_set_text_alignment(s_row_three_layer, GTextAlignmentLeft);
   
     // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_task_layer));
+  
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_row_one_layer));
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_row_two_layer));
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_row_three_layer));
 }
 
 static void main_window_load(Window *window) {  
@@ -66,22 +133,112 @@ static void main_window_load(Window *window) {
   declare_text_layers(window);
 }
 
+
 //Perform certain functions depending on what time it is
 static void update_task(struct tm *tick_time) {
-    static char hoursStr[] = "00";
-    strftime(hoursStr, sizeof("00"), "%H", tick_time);
-    int hours = atoi(hoursStr);
-    //strftime(dayChecker, sizeof("0"), "%w", tick_time);
-  if (hours >= 8 && hours <= 16)
+  int hours = get_hours(tick_time);
+  int minutes = get_minutes(tick_time);
+  int day = get_days(tick_time);
+
+  //strftime(dayChecker, sizeof("0"), "%w", tick_time);
+  if (hours >= 8 && (hours <= 15 && minutes <=59)  && day >= 1 && day <= 5)
   {
     text_layer_set_text(s_task_layer, "WORK TIME");
-  } else if (hours >= 16 && hours <=17) {
+  } 
+  else if (hours == 16 && minutes <=59 && day >= 1 && day <= 5) 
+  {
     text_layer_set_text(s_task_layer, "TRAVEL TIME");
-  } else if (hours >=17 && hours <=18){
+  } 
+  else if ((day == 0 && hours >= 10 && hours < 15) || ((hours >=17  && minutes <=59) && (day == 1 || day == 3 || day == 4))) 
+  {
     text_layer_set_text(s_task_layer, "GYM TIME");
   }
-  else{
+  else
+  {
     text_layer_set_text(s_task_layer, "FREE TIME");
+  }
+}
+
+static void set_row_one(struct tm *tick_time) {
+  int minutes = get_minutes(tick_time);
+  static char hoursStr[] = "00";
+  strftime(hoursStr, sizeof("00"), "%H", tick_time);
+  
+  if(minutes > 56 || minutes <= 1) {
+    text_layer_set_text(s_row_one_layer, hoursStr);
+  }
+  if(minutes > 1 && minutes < 6) {
+    text_layer_set_text(s_row_one_layer, "FIVE");
+  }
+  if(minutes > 6 && minutes < 11) {
+    text_layer_set_text(s_row_one_layer, "TEN");
+  }
+    if(minutes > 11 && minutes < 16) {
+    text_layer_set_text(s_row_one_layer, "QUARTER");
+  }
+  if(minutes > 16 && minutes < 21) {
+    text_layer_set_text(s_row_one_layer, "TWENTY");
+  }
+  if(minutes > 21 && minutes < 26) {
+    text_layer_set_text(s_row_one_layer, "TWENTY");
+  }
+  if(minutes > 26 && minutes < 31) {
+    text_layer_set_text(s_row_one_layer, "HALF");
+  }
+  if(minutes > 31 && minutes < 36) {
+    text_layer_set_text(s_row_one_layer, "THIRTY");
+  }
+  if(minutes > 36 && minutes < 41) {
+    text_layer_set_text(s_row_one_layer, "TWENTY");
+  }
+  if(minutes > 41 && minutes < 46) {
+    text_layer_set_text(s_row_one_layer, "QUARTER");
+  }
+  if(minutes > 46 && minutes < 51) {
+    text_layer_set_text(s_row_one_layer, "TEN");
+  }
+  if(minutes > 51 && minutes < 56) {
+    text_layer_set_text(s_row_one_layer, "FIVE");
+  }
+}
+
+static void set_row_two(struct tm *tick_time) {
+  int minutes = get_minutes(tick_time);
+  static char hoursStr[] = "00";
+  strftime(hoursStr, sizeof("00"), "%H", tick_time);
+  
+  if(minutes > 56 || minutes <= 1) {
+    text_layer_set_text(s_row_two_layer, hoursStr);
+  }
+  if(minutes > 1 && minutes < 6) {
+    text_layer_set_text(s_row_two_layer, "O'");
+  }
+  if(minutes > 1 && minutes < 21) {
+    text_layer_set_text(s_row_two_layer, "PAST");
+  }
+  if(minutes > 21 && minutes < 26) {
+    text_layer_set_text(s_row_two_layer, "FIVE PAST");
+  }
+  if(minutes > 26 && minutes < 31) {
+    text_layer_set_text(s_row_two_layer, "HALF");
+  }
+  if(minutes > 31 && minutes < 36) {
+    text_layer_set_text(s_row_two_layer, "FIVE PAST");
+  }
+  if(minutes > 36 && minutes < 56) {
+    text_layer_set_text(s_row_two_layer, "TO");
+  }
+}
+
+static void set_row_three(struct tm *tick_time) {
+  int minutes = get_minutes(tick_time);
+  char * hoursStr = get_hour_str(tick_time);
+  
+  if(minutes > 56 || minutes <= 1) {
+    text_layer_set_text(s_row_three_layer, "CLOCK");
+  }
+  else {
+     text_layer_set_text(s_row_three_layer, hoursStr);
   }
 }
 
@@ -91,8 +248,7 @@ static void update_time() {
   struct tm *tick_time = localtime(&temp);
 
   // Create a long-lived buffer
-  static char buffer[] = "00:00";
-
+  static char buffer[] = "0000";
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
     // Use 24 hour format
@@ -103,7 +259,12 @@ static void update_time() {
     strftime(buffer, sizeof("0000"), "%I%M", tick_time);
   }
   // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, buffer);
+  //text_layer_set_text(s_time_layer, buffer);
+  set_row_one(tick_time);
+  set_row_two(tick_time);
+  set_row_three(tick_time);
+  
+
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -121,6 +282,12 @@ static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_task_layer);
+  
+   text_layer_destroy(s_row_one_layer);
+   text_layer_destroy(s_row_two_layer);
+   text_layer_destroy(s_row_three_layer);
+  
+  
 }
   
 static void init() {
