6043c1246c35879ebb783b1368839e075d93d656
Scott <gloriousCode@users.noreply.github.com>
2015-07-30 20:43:47 +1000

    Now with weather!
    Gets weather information from Sydney (I don't care for getting precise location its all the same)
    Formats code a little

--- src/main.c
+++ src/main.c
@@ -22,6 +22,11 @@ static GPathInfo s_task_color_path_info = {
   .points = (GPoint[]) { {00, 00},  {144, 00}, {144, 34}, {00, 34} }
 };
 
+//Cool sys variables
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+
+
 //Bools
 bool currentlyWorkTime = false;
 bool currentlyPrepTime = false;
@@ -465,6 +470,51 @@ static void main_window_load(Window *window) {
   add_text_layers_to_window(window);
   text_layer_set_text(s_weather_layer, "28");
 }
+
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+static char temperature_buffer[8];
+static char conditions_buffer[32];
+static char weather_layer_buffer[32];
+// Read first item
+  Tuple *t = dict_read_first(iterator);
+
+  // For all items
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+   case KEY_TEMPERATURE:
+  snprintf(temperature_buffer, sizeof(temperature_buffer), "%d*", (int)t->value->int32);
+  break;
+case KEY_CONDITIONS:
+  snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+  break;
+default:
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+  break;
+    }
+    // Look for next item
+    t = dict_read_next(iterator);
+  }
+  // Assemble full string and display
+  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer),  temperature_buffer);
+  text_layer_set_text(s_weather_layer, weather_layer_buffer);
+}
+
+
   
 static void init() {
   // Create main Window element and assign to pointer
@@ -476,6 +526,16 @@ static void init() {
     .load = main_window_load,
     .unload = main_window_unload
   });
+  
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
   // Make sure the time is displayed from the start
