513a3bc0e13575b2477d3804d3e07a8620e324dd
Scott <gloriousCode@users.noreply.github.com>
2015-08-16 17:12:53 +1000

    Meditation, Travel prep times added
    Now has a dedicated time and task for meditation
    When travel or prep modes are activated, train times will appear. Will try and utilise the four rows to their fullest

--- src/main.c
+++ src/main.c
@@ -10,12 +10,19 @@ static TextLayer *s_row_three_layer;
 static TextLayer *s_weather_layer;
 static TextLayer *s_day_of_month_layer;
 
+static TextLayer *s_travel_row_one_layer;
+static TextLayer *s_travel_row_two_layer;
+static TextLayer *s_travel_row_three_layer;
+static TextLayer *s_travel_row_four_layer;
+
 static GFont s_time_font;
 static GFont s_task_font;
 static GFont s_text_time_font;
 static GFont s_text_time_bold_font;
 static GFont s_weather_font;
 static GFont s_day_of_month_font;
+static GFont s_travel_header_font;
+static GFont s_travel_row_font;
 
 static Layer *s_task_color_layer;
 static Layer *s_weather_icon_layer;
@@ -40,6 +47,7 @@ bool currentlyStudyTime = false;
 bool currentlyReadTime = false;
 bool currentlyTravelTime = false;
 bool currentlyFreeTime = false;
+bool currentlyMeditating = false;
 
 //Constants
 const char * clock = "clock";
@@ -74,6 +82,7 @@ const char * studyTime = "STUDY";
 const char * readTime = "READ";
 const char * travelTime = "MOVE";
 const char * freeTime = "FREE";
+const char * meditate = "MUSE";
 
 const char * strEmpty = "";
 
@@ -261,6 +270,8 @@ static void set_fonts() {
   s_text_time_bold_font = fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD);
   s_weather_font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
   s_day_of_month_font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+  s_travel_header_font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+  s_travel_row_font = fonts_get_system_font(FONT_KEY_GOTHIC_24);
 }
 
 static void set_text_layer_bounds() {
@@ -292,8 +303,25 @@ static void set_text_layer_bounds() {
   s_day_of_month_layer = text_layer_create(GRect(114, 2, 25, 25));
   text_layer_set_background_color(s_day_of_month_layer, GColorClear);
   text_layer_set_text_color(s_day_of_month_layer, GColorBlack);
-
+  //Create travel header row layer
+  s_travel_row_one_layer = text_layer_create(GRect(5, 70, 139, 50));
+  text_layer_set_background_color(s_travel_row_one_layer, GColorClear);
+  text_layer_set_text_color(s_travel_row_one_layer, GColorWhite);
+  //Create travel data layer two
+  s_travel_row_two_layer = text_layer_create(GRect(5, 90, 139, 50));
+  text_layer_set_background_color(s_travel_row_two_layer, GColorClear);
+  text_layer_set_text_color(s_travel_row_two_layer, GColorWhite);
+  //Create travel data layer three
+  s_travel_row_three_layer = text_layer_create(GRect(5, 110, 139, 50));
+  text_layer_set_background_color(s_travel_row_three_layer, GColorClear);
+  text_layer_set_text_color(s_travel_row_three_layer, GColorWhite);
+  //Create travel data layer four
+  s_travel_row_four_layer = text_layer_create(GRect(5, 130, 139, 50));
+  text_layer_set_background_color(s_travel_row_four_layer, GColorClear);
+  text_layer_set_text_color(s_travel_row_four_layer, GColorWhite);
 }
+
+
 
 static void apply_fonts_set_alignment()  {
   // Apply to TextLayer
@@ -304,7 +332,11 @@ static void apply_fonts_set_alignment()  {
   text_layer_set_font(s_row_two_layer, s_text_time_font);
   text_layer_set_font(s_row_three_layer, s_text_time_font);
   text_layer_set_font(s_weather_layer, s_weather_font);
-   text_layer_set_font(s_day_of_month_layer, s_day_of_month_font);
+  text_layer_set_font(s_day_of_month_layer, s_day_of_month_font);
+  text_layer_set_font(s_travel_row_one_layer, s_travel_header_font);
+  text_layer_set_font(s_travel_row_two_layer, s_travel_row_font);
+  text_layer_set_font(s_travel_row_three_layer, s_travel_row_font);
+  text_layer_set_font(s_travel_row_four_layer, s_travel_row_font);
   
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   text_layer_set_text_alignment(s_task_layer, GTextAlignmentLeft);
@@ -314,6 +346,11 @@ static void apply_fonts_set_alignment()  {
   text_layer_set_text_alignment(s_row_three_layer, GTextAlignmentLeft);
   text_layer_set_text_alignment(s_weather_layer, GTextAlignmentLeft);
   text_layer_set_text_alignment(s_day_of_month_layer, GTextAlignmentCenter);
+  //Travel time rows
+  text_layer_set_text_alignment(s_travel_row_one_layer, GTextAlignmentLeft);
+  text_layer_set_text_alignment(s_travel_row_two_layer, GTextAlignmentLeft);
+  text_layer_set_text_alignment(s_travel_row_three_layer, GTextAlignmentLeft);
+  text_layer_set_text_alignment(s_travel_row_four_layer, GTextAlignmentLeft);
 }
 
 static void add_small_time_layer_to_window(Window *window){
@@ -333,6 +370,11 @@ static void add_text_layers_to_window(Window *window) {
   add_text_time_layers_to_window(window);
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_day_of_month_layer));
+  
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_travel_row_one_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_travel_row_two_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_travel_row_three_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_travel_row_four_layer));
 }
 
 static void setTasksToFalse() {
@@ -344,6 +386,21 @@ static void setTasksToFalse() {
   currentlyReadTime = false;
   currentlyTravelTime = false;
   currentlyFreeTime = false;
+  currentlyMeditating = false;
+}
+
+static void set_travel_row_text() {
+  text_layer_set_text(s_travel_row_one_layer,   "M20   BER   GOS");
+  text_layer_set_text(s_travel_row_two_layer,   "355   415C  415C");
+  text_layer_set_text(s_travel_row_three_layer, "407   432R  432R");
+  text_layer_set_text(s_travel_row_four_layer,  "417   445C  445C");
+}
+
+static void clear_travel_row_text() {
+  text_layer_set_text(s_travel_row_one_layer, strEmpty);
+  text_layer_set_text(s_travel_row_two_layer, strEmpty);
+  text_layer_set_text(s_travel_row_three_layer, strEmpty);
+  text_layer_set_text(s_travel_row_four_layer, strEmpty);
 }
 
 //Perform certain functions depending on what time it is
@@ -357,10 +414,12 @@ static void update_task(struct tm *tick_time) {
       text_layer_set_text(s_task_layer, prepTime);
       taskColour = GColorPurple;
       layer_set_update_proc(s_task_color_layer, task_background_color);
+      set_travel_row_text();
     }
   } else if ((hours >= 8 && hours <= 15) && days >= 1 && days <= 5)
   {
-    if(!currentlyWorkTime) {   
+    if(!currentlyWorkTime) {
+      text_layer_set_text(s_time_layer, strEmpty);
       vibes_double_pulse();
       setTasksToFalse();
       currentlyWorkTime = true;
@@ -380,11 +439,13 @@ static void update_task(struct tm *tick_time) {
       text_layer_set_text(s_task_layer, travelTime);
       taskColour = GColorOrange;
       layer_set_update_proc(s_task_color_layer, task_background_color);
+      set_travel_row_text();
     }
   } 
   else if ((days == 0 && (hours >= 10 && hours < 15)) || (hours >=17  && minutes <=59 && hours <=19 && (days == 1 || days == 3 || days == 4))) 
   {
     if(!currentlyGymTime) {
+      text_layer_set_text(s_time_layer, strEmpty);
       vibes_double_pulse();
       setTasksToFalse();
       currentlyGymTime = true;
@@ -392,11 +453,27 @@ static void update_task(struct tm *tick_time) {
       text_layer_set_text(s_task_layer, gymTime);
       taskColour = GColorRed;
       layer_set_update_proc(s_task_color_layer, task_background_color);
+      clear_travel_row_text();
+    }
+  }
+  else if(hours == 20 && (minutes >= 29 || minutes <= 49))
+    {
+     if(!currentlyMeditating) {
+      text_layer_set_text(s_time_layer, strEmpty);
+      vibes_double_pulse();
+      setTasksToFalse();
+      currentlyMeditating = true;
+      
+      text_layer_set_text(s_task_layer, meditate);
+      taskColour = GColorMelon;
+      layer_set_update_proc(s_task_color_layer, task_background_color);
+       clear_travel_row_text();
     }
   }
   else 
   {
     if(!currentlyFreeTime) {
+      text_layer_set_text(s_time_layer, strEmpty);
       vibes_double_pulse();
       setTasksToFalse();
       currentlyFreeTime = true;
@@ -404,6 +481,7 @@ static void update_task(struct tm *tick_time) {
       text_layer_set_text(s_task_layer,freeTime);
       taskColour = GColorIslamicGreen;
       layer_set_update_proc(s_task_color_layer, task_background_color);
+      clear_travel_row_text();
     }
   }
 }
@@ -477,13 +555,6 @@ static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
-  // Create a long-lived buffer
-
-  // Write the current hours and minutes into the buffer
-  // Use 24 hour format
-  strftime(buffer, sizeof("0000"), "%H%M", tick_time);
-
- 
   //Set all the time based values
   set_hours(tick_time);
   set_minutes(tick_time);
@@ -492,22 +563,14 @@ static void update_time() {
   set_day_of_month(tick_time);
   snprintf(dayOfMonthTextBuffer, sizeof(dayOfMonthTextBuffer), "%d", dayOfMonth);
   text_layer_set_text(s_day_of_month_layer, dayOfMonthTextBuffer);
-  
-  
   update_task(tick_time);
-  
-  if ((hours >= 15 && hours <= 16) && minutes >44 && days >= 1 && days <= 5) {
-    //clear text time
+  if (((hours >= 15 && hours <= 16) && minutes >44 && days >= 1 && days <= 5) || (hours == 16 && minutes <=59 && days >= 1 && days <= 5)  ) {
+    //Clear text time and make it smaller for data
     text_layer_set_text(s_row_one_layer, strEmpty);
     text_layer_set_text(s_row_two_layer, strEmpty);
     text_layer_set_text(s_row_three_layer, strEmpty);
+    strftime(buffer, sizeof("0000"), "%H%M", tick_time);
     text_layer_set_text(s_time_layer, buffer);
-  }
-  else if (hours >= 16 && minutes >=15  && days >= 1 && days <= 5) {
-    text_layer_set_text(s_time_layer, strEmpty);
-    set_row_one();
-    set_row_two();
-    set_row_three();
   }
   else {
     set_row_one();
@@ -552,6 +615,11 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_row_three_layer);
   text_layer_destroy(s_weather_layer);
   text_layer_destroy(s_day_of_month_layer);
+  
+  text_layer_destroy(s_travel_row_one_layer);
+  text_layer_destroy(s_travel_row_two_layer);
+  text_layer_destroy(s_travel_row_three_layer);
+  text_layer_destroy(s_travel_row_four_layer);
 }
 
 static void main_window_load(Window *window) {   
@@ -582,7 +650,6 @@ static char temperature_buffer[8];
 static char conditions_buffer[32];
 static char condition_layer_buffer[32];
 static char weather_layer_buffer[32];
-
 // Read first item
   Tuple *t = dict_read_first(iterator);
 
@@ -622,22 +689,18 @@ static void init() {
     .load = main_window_load,
     .unload = main_window_unload
   });
-  
   // Register callbacks
   app_message_register_inbox_received(inbox_received_callback);
   // Open AppMessage
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-
   app_message_register_inbox_dropped(inbox_dropped_callback);
   app_message_register_outbox_failed(outbox_failed_callback);
   app_message_register_outbox_sent(outbox_sent_callback);
-  
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
   // Make sure the time is displayed from the start
   update_time();
   // Register with TickTimerService
-
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 
 }
